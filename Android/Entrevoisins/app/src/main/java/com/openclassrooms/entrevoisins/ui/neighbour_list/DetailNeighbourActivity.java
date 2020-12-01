a0510368c520cb15555b128ef54e87f1b631fd8c
Guillaume Toussaint <gtdev.git@gmail.com>
2020-12-01 09:53:22 +0100

    Ajout détail d'un voisin

---
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.entrevoisins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailNeighbourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_neighbour);
+    }
+}
