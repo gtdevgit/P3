c633790fa75c1e370fda597721d1ff331f0fd08b
Guillaume Toussaint <gtdev.git@gmail.com>
2020-12-02 10:54:44 +0100

    test detail neighbour 2

---
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity2.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailNeighbourActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_neighbour2);
+    }
+}
